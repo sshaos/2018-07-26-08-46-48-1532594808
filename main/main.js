6fbc161ad56f3f502e5e3a89301b360012681acb
shaoshuai <shaoshuai@hust.edu.cn>
2018-07-29 11:21:13 +0800

    add main funcation

--- main/main.js
+++ main/main.js
@@ -1,18 +1,27 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.size}
+o) 平均值 = ${sequence.average()}`);
 };
-
 class Sequence {
   constructor(input) {
-    // Write your code here
+    this.input = input;
+    this.size = input.length;
   }
-
   minimum() {
-    // Write your code here
+    return Math.min(...this.input);//ES6扩展特性
   }
-
-  // Write your code here
+  maximum() {
+    return Math.max(...this.input);//ES6扩展特性
+  }
+  average() {
+    var sum = 0;
+    for(var i =0;i<this.size;i++)
+    {
+        sum += this.input[i];
+    } 
+    return (sum/this.size).toFixed(2);
+  }
 }
